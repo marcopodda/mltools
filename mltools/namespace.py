aa8275547da00311dc844a27a1fe327db506257b
Marco Podda <32294463+marcopodda@users.noreply.github.com>
2021-05-04 09:32:05 +0200

    Update namespace.py

--- mltools/namespace.py
+++ mltools/namespace.py
@@ -2,7 +2,7 @@ from argparse import Namespace
 from .serialization import load_yaml, save_yaml
 
 
-class NamespaceDict(Namespace):
+class Options(Namespace):
     @classmethod
     def from_dict(cls, attrdict):
         return cls(**attrdict)
@@ -26,3 +26,18 @@ class NamespaceDict(Namespace):
     def __contains__(self, key):
         attrdict = self.to_dict()
         return key in attrdict
+    
+    def update(self, *args, **kwargs):
+        for i, obj in enumerate(args, 1):
+            if isinstance(obj, Namespace):
+                obj = vars(obj)
+            elif isinstance(obj, Mapping):
+                obj = dict(obj)
+            else:
+                raise TypeError(f"Unsupported type for positional argument #{i}: {type(obj)}")
+
+            for name, value in obj.items():
+                self.__dict__[name] = value
+
+        for name, value in kwargs.items():
+            self.__dict__[name] = value
